f4d09c2d68241fb338d0eaf8aa11e2e5336b8ddd
Aadi Poddar <aadipoddarmail@gmail.com>
2021-08-08 15:11:41 +0530

    GLM Installation

--- OpenGL_Learn4/src/main.cpp
+++ OpenGL_Learn4/src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 void framebuffer_size_callback ( GLFWwindow * window , int width , int height );
 void processInput ( GLFWwindow * window );
@@ -8,6 +11,15 @@ void processInput ( GLFWwindow * window );
 int main ( )
 {
 	std::cout << "Hello World" << std::endl;
+
+    //glm test
+    glm::vec4 vec ( 1.0f , 0.0f , 0.0f , 1.0f );
+    glm::mat4 trans = glm::mat4 ( 1.0f );// identity matrix
+    trans = glm::translate ( trans , glm::vec3 ( 1.0f , 1.0f , 0.0f ) );
+    trans = glm::rotate ( trans , glm::radians ( 90.0f ) , glm::vec3 ( 0.0 , 0.0 , 1.0 ) );
+    trans = glm::scale ( trans , glm::vec3 ( 0.5 , 0.5 , 0.5 ) );
+    vec = trans * vec;
+    std::cout << vec.x << vec.y << vec.z << std::endl;
 
     glfwInit ( );
 
